e489f9b50fad8f8bd4ac85f7e4fe9d37adcdaf85
Julio B <julio.bacel@gmail.com>
2018-11-27 23:43:19 +0200

    Add osmdroid map

--- app/src/main/java/unipi/sem7/unipimeter/MainActivity.java
+++ app/src/main/java/unipi/sem7/unipimeter/MainActivity.java
@@ -1,6 +1,8 @@
 package unipi.sem7.unipimeter;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
@@ -16,16 +18,27 @@ import android.view.MenuItem;
 
 import com.github.anastr.speedviewlib.ProgressiveGauge;
 
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.ITileSource;
+import org.osmdroid.tileprovider.tilesource.XYTileSource;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
 import unipi.sem7.unipimeter.dummy.DummyContent.DummyItem;
 
 public class MainActivity extends AppCompatActivity implements POIFragment.OnListFragmentInteractionListener {
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
     public ProgressiveGauge speedometerGauge;
+    private MapView mMapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -37,6 +50,22 @@ public class MainActivity extends AppCompatActivity implements POIFragment.OnLis
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+        mMapView = (MapView) findViewById(R.id.osmdroid);
+        mMapView.setMultiTouchControls(true);
+        mMapView.setMinZoomLevel(1.4);
+        mMapView.setMaxZoomLevel(18.9);
+        mMapView.setTilesScaledToDpi(true);
+        mMapView.getController().setZoom(8.0);
+        mMapView.getController().setCenter(new GeoPoint(37.941649, 23.652894));
+
+        final ITileSource tileSource = new XYTileSource( "Dark", 1, 18, 256, ".png",
+                new String[] {
+                        "https://a.basemaps.cartocdn.com/dark_all/",
+                        "https://b.basemaps.cartocdn.com/dark_all/",
+                        "https://c.basemaps.cartocdn.com/dark_all/",
+                        "https://d.basemaps.cartocdn.com/dark_all/" });
+        mMapView.setTileSource(tileSource);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
